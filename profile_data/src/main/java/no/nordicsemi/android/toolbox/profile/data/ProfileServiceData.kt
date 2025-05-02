ca7395b2e65c56a9d701168d16cdac901a2dcf23
Schmackiez <knickiz@icloud.com>
2025-05-03 00:35:02 +1200

    Delete profile_data directory

--- profile_data/src/main/java/no/nordicsemi/android/toolbox/profile/data/ProfileServiceData.kt
+++
@@ -1,8 +0,0 @@
-package no.nordicsemi.android.toolbox.profile.data
-
-/**
- * Profile service data class that holds the profile and the service data.
- */
-sealed class ProfileServiceData {
-    abstract val profile: Profile
-}
--- profile_data/src/main/java/no/nordicsemi/android/toolbox/profile/data/uart/UARTMacro.kt
+++
@@ -1,7 +0,0 @@
-package no.nordicsemi.android.toolbox.profile.data.uart
-
-data class UARTMacro(
-    val icon: MacroIcon,
-    val command: String?,
-    val newLineChar: MacroEol
-)
